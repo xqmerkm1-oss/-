1944ca87c3e595c1d9983380331c883725df876b
xqmerkm1-oss <xqmerkm1@gmail.com>
2026-09-21 21:54:19 +0400

    Update config.py

--- config.py
+++ config.py
@@ -23,18 +23,14 @@ HIGH_POINT_REWARD = 1         # بالای ۵۰۰۰۰ — سلام گلم
 TOP_POINT_REWARD = 1          # بالای ۲۰۰۰۰۰ — کیک
 
 KIR_POINT_COOLDOWN = 180      # ۳ دقیقه
-KOS_POINT_COOLDOWN = 180
-WEAK_POINT_COOLDOWN = 180
-HIGH_POINT_COOLDOWN = 180
-TOP_POINT_COOLDOWN = 180
 
 # ===== کلمات کلیدی =====
 KIR_WORD = "کیر"
 KOS_WORD = "کص"
-MALE_GOOD_WORD = "پسر خوب"     # برای پسر بی‌جنبه
-FEMALE_GOOD_WORD = "دختر خوب"  # برای دختر بی‌جنبه
-HIGH_WORD = "سلام گلم"          # بالای ۵۰۰۰۰
-TOP_WORD = "کیک"               # بالای ۲۰۰۰۰۰
+MALE_GOOD_WORD = "پسر خوب"
+FEMALE_GOOD_WORD = "دختر خوب"
+HIGH_WORD = "سلام گلم"
+TOP_WORD = "کیک"
 
 # ===== آستانه‌ها =====
 HIGH_THRESHOLD = 50000
