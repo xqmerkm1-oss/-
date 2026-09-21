056f13ca02ac8a3b38884dc6c5de5a455b9664ce
xqmerkm1-oss <xqmerkm1@gmail.com>
2026-09-21 18:50:26 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # ===== مقادیر پیشفرض =====
-# این مقادیر اگه توی Variables نبودن، از اینجا استفاده میشن
 DEFAULT_BOT_TOKEN = "8845091304:AAGSunCBF1Ijfgyn_do2xjHyUxwjR_fWKpA"
 DEFAULT_CHANNEL_ID = "-1004344839671"
 DEFAULT_CHANNEL_LINK = "https://t.me/Afirstratescumbag"
 DEFAULT_DATABASE_PATH = "data/bot.db"
 
-# ===== خواندن از محیط یا پیشفرض =====
 BOT_TOKEN = os.getenv("BOT_TOKEN") or DEFAULT_BOT_TOKEN
 CHANNEL_ID = int(os.getenv("CHANNEL_ID") or DEFAULT_CHANNEL_ID)
 CHANNEL_LINK = os.getenv("CHANNEL_LINK") or DEFAULT_CHANNEL_LINK
