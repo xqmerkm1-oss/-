bf47674f6179ac96895f9c3b2a59820f0f7286aa
xqmerkm1-oss <xqmerkm1@gmail.com>
2026-09-22 19:09:37 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
+DATABASE_URL: str = os.getenv(
+    "DATABASE_URL",
+    "postgresql+asyncpg://shomped:shomped@db:5432/shomped",
+)
+
+if not BOT_TOKEN:
+    raise ValueError("❌ BOT_TOKEN تنظیم نشده.")
