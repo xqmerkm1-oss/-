7916df9dbc1a6c55dabaebf7f306e69a151fb7a8
xqmerkm1-oss <xqmerkm1@gmail.com>
2026-09-22 20:06:26 +0400

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -7,12 +7,14 @@ def start_keyboard() -> InlineKeyboardMarkup:
             InlineKeyboardButton(
                 "➕ افزودن به گروه",
                 url="https://t.me/Schompedbot?startgroup=true",
+                style="success",  # 🟢 سبز
             )
         ],
         [
             InlineKeyboardButton(
                 "📢 کانال اطلاع رسانی",
                 url="https://t.me/SchompedCanal",
+                style="primary",  # 🔵 آبی
             )
         ],
     ]
