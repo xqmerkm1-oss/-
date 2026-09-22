01b32392a7e9173683d5ef1a2c851f31b8f5747a
xqmerkm1-oss <xqmerkm1@gmail.com>
2026-09-22 20:16:13 +0400

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
 DATABASE_URL: str = os.getenv(
     "DATABASE_URL",
-    "postgresql+asyncpg://shomped:shomped@db:5432/shomped",
+    "8900397795:AAGcpe7E-qavZYXmIJlHsD4s9Li5_VkM2_8",
 )
 
 if not BOT_TOKEN:
